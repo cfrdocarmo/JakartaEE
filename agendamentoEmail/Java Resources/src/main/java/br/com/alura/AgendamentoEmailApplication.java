2075f21ce36ad079a668229bfd457f23fe64daa7
cfrdocarmo <73319579+cfrdocarmo@users.noreply.github.com>
2021-05-04 21:13:12 -0300

    Add files via upload

---
+++ agendamentoEmail/Java Resources/src/main/java/br/com/alura/AgendamentoEmailApplication.java
@@ -0,0 +1,9 @@
+package br.com.alura;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+public class AgendamentoEmailApplication extends Application{
+
+}
